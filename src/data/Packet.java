c8cd1d2ed27a2e66613b7b8851236e9f4f782201
jpyen <it>
2017-10-05 15:38:28 +0200

    Javadoc Comments

--- src/data/Packet.java
+++ src/data/Packet.java
@@ -11,6 +11,14 @@ public class Packet {
     private int weight;
     private double price;
 
+    /**
+     *
+     * @param width
+     * @param height
+     * @param length
+     * @param weight
+     * @param price
+     */
    public Packet(int width, int height, int length, int weight, int price){
         this.width = width;
         this.height = height;
@@ -18,25 +26,50 @@ public class Packet {
         this.weight = weight;
         this.price = price;
     }
+
+    /**
+     *
+     * @param width
+     * @param height
+     * @param length
+     * @param weight
+     */
     public Packet(int width, int height, int length, int weight){
         this.width = width;
         this.height = height;
         this.length = length;
         this.weight = weight;
+        this.price = 0;
     }
 
+    /**
+     *
+     * @param price
+     */
     public void setPrice(int price){
         this.price = price;
     }
 
+    /**
+     *
+     * @return
+     */
     public int getWeight(){
         return weight;
     }
 
+    /**
+     *
+     * @return
+     */
     public int getPrice(){
         return (int) price;
     }
 
+    /**
+     *
+     * @return
+     */
     public int[] getPackageDiameters(){
         int ret[] = {width, height, length, weight};
         return ret;
